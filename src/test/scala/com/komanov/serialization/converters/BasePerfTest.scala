46137195c1101949bda37e8f72f665f80802f884
Dmitry Komanov <dmitryk@wix.com>
2016-05-28 22:34:52 +0300

    Add recent times for serialization-deserialization

--- src/test/scala/com/komanov/serialization/converters/BasePerfTest.scala
+++ src/test/scala/com/komanov/serialization/converters/BasePerfTest.scala
@@ -64,10 +64,15 @@ trait BasePerfTest[Input, Output] {
 
 /*
 Converter,1k,2k,4k,8k,64k
-JSON,4483,9478,19129,40738,308020
-ScalaPB,2635,4632,9747,18381,152629
-Java PB,3562,6530,11926,22986,234368
-Pickles,61370,95536,248517,674345,4797155
+JSON,4365,8437,16771,35164,270175
+ScalaPB,2176,3936,7475,14822,133119
+Java PB,3173,6393,10123,21379,209716
+Java Thrift,3657,6805,13074,27667,261673
+Scrooge,3572,6506,12050,25036,233895
+Serializable,13156,21203,36457,79045,652942
+Pickles,53991,83601,220440,589888,4162785
+Boopickle,5451,10628,17533,29765,225717
+Chill,7202,9783,15130,27338,207871
  */
 object SerializationPerfTestApp extends App with BasePerfTest[Site, Array[Byte]] {
   override def createInput(converter: SiteConverter, site: Site): Site = site
@@ -79,10 +84,15 @@ object SerializationPerfTestApp extends App with BasePerfTest[Site, Array[Byte]]
 
 /*
 Converter,1k,2k,4k,8k,64k
-JSON,7885,12686,24804,54639,390988
-ScalaPB,2399,4325,8297,15774,134520
-Java PB,3367,6182,10893,20676,173379
-Pickles,41108,67136,176470,467082,3362515
+JSON,7670,12964,24804,51578,384623
+ScalaPB,2335,4576,7326,14754,128730
+Java PB,3504,6076,10269,19792,168952
+Java Thrift,3451,5812,10048,20693,176020
+Scrooge,3640,6522,12740,25081,230556
+Serializable,61455,84196,102870,126839,575232
+Pickles,40337,63840,165109,446043,3201348
+Boopickle,2848,5017,8454,15962,97270
+Chill,6675,9654,14770,25261,193136
  */
 object DeserializationPerfTestApp extends App with BasePerfTest[Array[Byte], Site] {
   override def createInput(converter: SiteConverter, site: Site): Array[Byte] = converter.toByteArray(site)
