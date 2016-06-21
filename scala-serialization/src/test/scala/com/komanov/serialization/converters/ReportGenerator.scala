7cb223a073a795f51f33178de50335e2d6567ed9
Dmitry Komanov <dmitryk@wix.com>
2016-06-21 20:27:01 +0300

    Upgrade pickling version from 0.11.0-M1 to 0.11.0-M2

--- scala-serialization/src/test/scala/com/komanov/serialization/converters/ReportGenerator.scala
+++ scala-serialization/src/test/scala/com/komanov/serialization/converters/ReportGenerator.scala
@@ -13,7 +13,7 @@ JSON,1060,2076,4043,8173,65835
 Protobuf,554,1175,1930,3058,27111
 Thrift,712,1441,2499,4315,38289
 Serializable,2207,3311,4549,6615,43168
-Pickles,1628,2883,5576,11762,97997
+Pickling,1681,2989,6000,13034,106901
 Boopickle,544,1130,1855,2882,16290
 Chill,908,1695,2507,3643,26261
 Data Sizes (gzip)
@@ -22,13 +22,13 @@ JSON,683,1136,1629,2681,11697
 Protobuf,486,893,1329,2177,9391
 Thrift,541,963,1400,2251,9612
 Serializable,1186,1740,2223,3044,10366
-Pickles,684,1142,1607,2561,10897
+Pickling,692,1144,1619,2579,11039
 Boopickle,468,858,1271,2076,9044
 Chill,601,1025,1455,2274,9193
  */
 object ReportGenerator extends App {
 
-  val flush = false
+  val flush = true
 
   val dir = new File(System.getProperty("user.home"), "123")
   require(!flush || dir.exists() || dir.mkdirs())
