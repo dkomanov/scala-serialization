2180f09cee25b72b9549031806a9275bda96b27e
Dmitry Komanov <dmitryk@wix.com>
2016-06-25 14:34:23 +0300

    Add script for running benchmarks

---
+++ jmh-run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+ec() {
+    echo $* 1>&2
+    $*
+}
+
+# expecting a gh-pages symlink in a root directory
+GH_PAGES_PATH=`readlink gh-pages`
+# ensure that git repository exists there
+ec git -C $GH_PAGES_PATH status > /dev/null
+
+ec mvn clean install
+
+ec java -jar scala-serialization-test/target/benchmarks.jar -rf json -rff jmh-result.json > jmh.log
+
+ec mv jmh-result.json $GH_PAGES_PATH
+
+echo "Don't forget to push gh-pages!"
